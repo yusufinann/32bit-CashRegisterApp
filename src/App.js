2b505534bb12f002fc26344e77ca6a0441a5b462
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-10 11:08:01 +0300

    Edit router operations

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
+
+
 import Home from "./HomePage/Home";
 import Login from "./LoginPage/Login";
 
 function App() {
   return (
+    <BrowserRouter>
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="/" element={<Login />} />
     </Routes>
+    </BrowserRouter>
   );
 }
 
