2157573580a961306db5e130a743161a47d1cd43
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-10 11:56:44 +0300

    Add React Router setup for login to App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { BrowserRouter } from 'react-router-dom';
-
-
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./HomePage/Home";
-import Login from "./LoginPage/Login";
+import LoginPage from "./LoginPage/Login";
 import { LoginProvider } from "./contexts/LoginContext";
 
 function App() {
   return (
-    <LoginProvider>
-    <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/" element={<Login />} />
-    </Routes>
-    </LoginProvider>
+    <BrowserRouter>
+      <LoginProvider>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+      </LoginProvider>
+    </BrowserRouter>
   );
 }
 
