4ea5215b859b7764dc6b2ebc8f0c558a4efe77ae
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-10 22:11:08 +0300

    create functions and states for categories

--- src/contexts/ClobalContext.js
+++ src/contexts/ClobalContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from "react";
 
 const GlobalContext = createContext();
 
@@ -10,8 +10,9 @@ const GlobalContextProvider = ({ children }) => {
   });
 
   const [state, setState] = useState({
-    barcode: '',
+    barcode: "",
     categories: [],
+    showCategories: false,
     // other properties specific to LeftSales...
   });
 
@@ -33,27 +34,51 @@ const GlobalContextProvider = ({ children }) => {
 
   const handleBarcodeChange = async (event) => {
     const newBarcode = event.target.value;
-    console.log('New barcode:', newBarcode);
+    console.log("New barcode:", newBarcode);
     // Additional logic for handling barcode change...
   };
 
- 
+  const handleShowCategories = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/categories");
+      if (!response.ok) {
+        throw new Error("API response was not ok.");
+      }
+      const data = await response.json();
+      setState((prevState) => ({
+        ...prevState,
+        categories: data,
+        showCategories: true,
+      }));
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  };
+
   const contextValue = {
     globalState,
     login,
     logout,
     state,
     handleBarcodeChange,
+
+    handleShowCategories,
     // other functions...
   };
 
-  return <GlobalContext.Provider value={contextValue}>{children}</GlobalContext.Provider>;
+  return (
+    <GlobalContext.Provider value={contextValue}>
+      {children}
+    </GlobalContext.Provider>
+  );
 };
 
 const useGlobalContext = () => {
   const context = useContext(GlobalContext);
   if (!context) {
-    throw new Error('useGlobalContext must be used within a GlobalContextProvider');
+    throw new Error(
+      "useGlobalContext must be used within a GlobalContextProvider"
+    );
   }
   return context;
 };
