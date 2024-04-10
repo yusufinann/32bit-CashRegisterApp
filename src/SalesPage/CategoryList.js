d2f62dad3fc2945218a29c14bbfce5981d2caa8f
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-10 22:19:59 +0300

    map the categories datas

--- src/SalesPage/CategoryList.js
+++ src/SalesPage/CategoryList.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import { Card, CardMedia, CardContent } from '@mui/material';
 
-const CategoryList = () => {
+const CategoryList = ({ categories }) => {
   return (
     <div>
       <div className="card-container">
+        {categories.map((category, index) => ( // index parametresini ekleyin
           <Card
+            key={`${category.category_id}-${index}`} // Her bir kategori için benzersiz bir anahtar
             className="custom-card"
             // onClick={() => handleShowProducts(category.category_id)} // Kategoriye tıklandığında ürünleri getir
           >
             <CardMedia
               component="img"
-            //   alt={category.category_name}
+              alt={category.category_name}
               height="140"
-            //   image={category.image_url}
+              image={category.image_url}
             />
             <CardContent>
-              {/* <p>{category.category_name}</p> */}
+              <p>{category.category_name}</p>
             </CardContent>
           </Card>
-    
+        ))}
       </div>
     </div>
   );
