fe7aa610ccf023b9cac5a5a9c2c44aa45db1813f
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-11 10:53:47 +0300

    Edit the ProductList component design and progress

--- src/SalesPage/ProductList.js
+++ src/SalesPage/ProductList.js
@@ -1,36 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Card, CardMedia, CardContent, Typography, Button } from '@mui/material';
 import './styles.css';
 import { useGlobalContext } from '../contexts/GlobalContext';
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
-const ProductList = ({ products, isOpen, toggle }) => {
-  const { addToCart } = useGlobalContext();
+const ProductList = ({ products }) => {
+  const {handleAddToCart,handleCardClick } = useGlobalContext();
 
   return (
     <div>
       <div className="card-container">
         {products.map((product) => (
-          <Card key={product.product_id} sx={{ width: 200 }} className="custom-card" onClick={() => addToCart(product)}>
+          <Card key={product.product_id} sx={{ width: 150 }} className="custom-card" onClick={() => handleCardClick(product)}>
             <CardMedia
               component="img"
               alt={product.product_name}
-              height="140"
+              height="150"
               image={product.image_url}
             />
             <CardContent>
-              <Typography variant="subtitle1" color="text.primary" fontWeight="bold">
+              <Typography variant="subtitle1" fontWeight="bold">
                 {product.product_name}
               </Typography>
-              <Typography variant="body2" color="text.primary">
+              <Typography variant="body2">
                 {product.price} TL
               </Typography>
               <Button
                 variant="contained"
                 color="primary"
                 startIcon={<AddShoppingCartIcon />}
+                onClick={(e) => {
+                  e.stopPropagation(); // Kartın onClick olayını durdur
+                  handleAddToCart(product);
+                }}
               >
-                Add
+                Sepete Ekle
               </Button>
             </CardContent>
           </Card>
@@ -38,6 +43,10 @@ const ProductList = ({ products, isOpen, toggle }) => {
       </div>
     </div>
   );
+};
+
+ProductList.propTypes = {
+  products: PropTypes.array.isRequired,
 };
 
 export default ProductList;
