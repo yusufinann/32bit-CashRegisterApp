aae8c519f286d4c7f48b5e17f011e8cade38bc0c
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-11 12:55:07 +0300

    edit and design in order summary page to progress

--- src/OrderSummary/OrderSummary.js
+++ src/OrderSummary/OrderSummary.js
@@ -1,40 +1,66 @@
 import React from 'react'
 import { Grid } from "@mui/material";
 import GlobalNavi from '../GlobalComponents/GlobalNavi';
-const OrderSummary = () => {
+const OrderSummary = () => { //see price
   return (
     <Grid container spacing={2}>
+     {/*Navi */}
+    <Grid
+      item
+      xs={12}
+      style={{  border: "1px solid #2b2d42" }}
+    >
+      <GlobalNavi  title="See Price" linkTo="/sales" />
+    </Grid>
     
     <Grid
-    item
-    xs={12}
-    style={{border: "1px solid #2b2d42" }}
-  >
-    <GlobalNavi  title="See Price" linkTo="/sales" />
-  </Grid>
-  
-  <Grid
-    item
-    xs={12} // Full width on small screens
-    sm={6}  // Half width on medium screens and larger
-    style={{
-      margin: '10px auto', // Center the grid
-      border: "1px solid #2b2d42",
-      borderRadius: 20,
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      height: "80vh",
-      
-    }}
-  >
-    <h2>See Price</h2>
-  </Grid>
-    
- 
-</Grid>
-)
+        item
+        xs={12} // Full width on small screens
+        sm={4} // Five columns on medium screens and larger
+        style={{
+          margin: 10,
+          border: "1px solid #2b2d42",
+          borderRadius: 20,
+          height: "85vh", // Set a fixed height
+          overflow: "auto", // Enable scrolling
+        }}
+      >
+        {/* <ReceiptArea/> */}
+      </Grid>
+
+      {/* CartList */}
+      <Grid
+        item
+        xs={12} // Full width on small screens
+        sm={4} // Four columns on medium screens and larger
+        style={{
+          margin: 10,
+          border: "1px solid #2b2d42",
+          borderRadius: 20,
+          height: "85vh", // Set a fixed height
+          
+        }}
+      >
+        {/* middle area for order summary */}
+      </Grid>
+
+      <Grid
+        item
+        xs={12} // Full width on small screens
+        sm={3} // Two columns on medium screens and larger
+        style={{
+          margin: 10,
+          border: "1px solid #2b2d42",
+          borderRadius: 20,
+          height: "85vh", // Set a fixed height
+        }}
+      >
+        <div>
+          {/* component */}
+        </div>
+      </Grid>
+    </Grid>
+  )
 }
 
 export default OrderSummary
