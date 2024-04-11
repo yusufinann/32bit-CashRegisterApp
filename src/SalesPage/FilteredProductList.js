3ce737a4382174f5afc2c99d09e312bba6374fe4
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-11 11:24:07 +0300

    Edit the FilteredProductList component design and progress

--- src/SalesPage/FilteredProductList.js
+++ src/SalesPage/FilteredProductList.js
@@ -1,36 +1,61 @@
 import React from 'react';
-import { useGlobalContext } from '../contexts/GlobalContext'
-import { Typography, Card, CardContent, Grid } from '@mui/material';
-
-const FilteredProductList = () => {
-  const { state } = useGlobalContext();
-
-  return (
-    <div>
-      <Typography variant="h5" gutterBottom>
-        Filtered Products
-      </Typography>
-      <Grid container spacing={2}>
-        {state.filteredProducts.map(product => (
-          <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
-            <Card>
-              <CardContent>
-                <Typography variant="h6" gutterBottom>
-                  {product.name}
-                </Typography>
-                <Typography variant="body1">
-                  Price: {product.price} TL
-                </Typography>
-                <Typography variant="body1">
-                  Barcode: {product.barcode}
-                </Typography>
+    import PropTypes from 'prop-types';
+    import { Typography, Card, CardContent, CardMedia, Button } from '@mui/material'; // Import Material-UI components
+    import { useGlobalContext } from '../contexts/GlobalContext';
+    import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+    import './styles.css';
+    
+    const FilteredProductList = () => {
+        const { state, addToCart } = useGlobalContext();
+      
+        const handleAddToCart = (product) => {
+          addToCart(product);
+        };
+      
+        const handleCardClick = (product) => {
+          addToCart(product);
+        };
+    
+      return (
+        <div>
+          <div className="card-container">
+            {state.filteredProducts.map((product, index) => (
+              <Card key={product.product_id} sx={{ width: 200 }} className="custom-card" onClick={() => handleCardClick(product)}>
+                <CardMedia
+                  component="img"
+                  alt={product.product_name}
+                  height="140"
+                  image={product.image_url}
+                />
+                <CardContent>
+                  <Typography variant="subtitle1" color="text.primary" fontWeight="bold">
+                    {product.name}
+                  </Typography>
+                  <Typography variant="body1">
+                    Price: {product.price} TL
+                  </Typography>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    startIcon={<AddShoppingCartIcon />}
+                    onClick={(e) => {
+                      e.stopPropagation(); // Stop the card's onClick event
+                      handleAddToCart(product);
+                    }}
+                  >
+                    Sepete Ekle
+                  </Button>
                 </CardContent>
-                </Card>
-              </Grid>
+              </Card>
             ))}
-          </Grid>
+          </div>
         </div>
       );
     };
     
+    FilteredProductList.propTypes = {
+      filteredProducts: PropTypes.array.isRequired,
+    };
+    
     export default FilteredProductList;
+    
