0bfe59a343787a4363dca8ffaef2f201dfe314cf
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-12 14:58:49 +0300

    Call globalcard list and update productlist

--- src/SalesPage/ProductList.js
+++ src/SalesPage/ProductList.js
@@ -1,46 +1,18 @@
+
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, CardMedia, CardContent, Typography, Button } from '@mui/material';
 import './styles.css';
 import { useGlobalContext } from '../contexts/GlobalContext';
-import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import GlobalCardList from '../GlobalComponents/GlobalCardList';
+
+
 
 const ProductList = ({ products }) => {
-  const {handleAddToCart,handleCardClick } = useGlobalContext();
+  const {handleAddToCart } = useGlobalContext();
 
   return (
     <div>
-      <div className="card-container">
-        {products.map((product) => (
-          <Card key={product.product_id} sx={{ width: 150 }} className="custom-card" onClick={() => handleCardClick(product)}>
-            <CardMedia
-              component="img"
-              alt={product.product_name}
-              height="150"
-              image={product.image_url}
-            />
-            <CardContent>
-              <Typography variant="subtitle1" fontWeight="bold">
-                {product.product_name}
-              </Typography>
-              <Typography variant="body2">
-                {product.price} TL
-              </Typography>
-              <Button
-                variant="contained"
-                color="primary"
-                startIcon={<AddShoppingCartIcon />}
-                onClick={(e) => {
-                  e.stopPropagation(); // Kartın onClick olayını durdur
-                  handleAddToCart(product);
-                }}
-              >
-                Sepete Ekle
-              </Button>
-            </CardContent>
-          </Card>
-        ))}
-      </div>
+        <GlobalCardList array={products} AddToCartFunction={handleAddToCart} />
     </div>
   );
 };
