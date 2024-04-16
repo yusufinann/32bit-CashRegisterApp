0f924eed3c2ea0631ff0dd9954114b363877680b
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-16 22:14:25 +0300

    call the CampaignList Modal's functions from the context

--- src/SalesPage/CampaignListModal.js
+++ src/SalesPage/CampaignListModal.js
@@ -1,32 +1,27 @@
 import React from 'react';
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
+import GlobalCardList from '../GlobalComponents/GlobalCardList';
+import { useGlobalContext } from '../contexts/GlobalContext';
 
 
 
-const CampaignListModal = ({ open, handleClose }) => {
-  
+const CampaignListModal = () => {
+    const { openCampaignModal, closeCampaignModalFn, handleAddToCart, handleCampaignFilter, filteredCampaignProducts } = useGlobalContext();
+
 
   return (
-    <Dialog open={open} onClose={() => { handleClose();}} aria-labelledby="campaign-list-dialog-title">
-      <DialogTitle id="campaign-list-dialog-title">Kampanya Listesi</DialogTitle>
-      <DialogContent>
-        {/* <GlobalCardList array={filteredCampaignProducts} AddToCartFunction={handleAddToCart} /> */}
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={() =>{}} color="primary">
-          3 Al 2 Öde
-        </Button>
-        <Button onClick={() => {}} color="secondary">
-          Yüzde 50 Indirim
-        </Button>
-        <Button onClick={() => {}} color="secondary">
-          Yüzde 10 Indirim
-        </Button>
-        <Button onClick={() => {}} color="primary">
-          Close
-        </Button>
-      </DialogActions>
-    </Dialog>
+    <Dialog open={openCampaignModal} onClose={closeCampaignModalFn} aria-labelledby="campaign-list-dialog-title">
+    <DialogTitle id="campaign-list-dialog-title">Kampanya Listesi</DialogTitle>
+    <DialogContent>
+      <GlobalCardList array={filteredCampaignProducts} AddToCartFunction={handleAddToCart} />
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={() => handleCampaignFilter('C001')} color="primary">3 Al 2 Öde</Button>
+      <Button onClick={() => handleCampaignFilter('C002')} color="secondary">Yüzde 50 Indirim</Button>
+      <Button onClick={() => handleCampaignFilter('C003')} color="secondary">Yüzde 10 Indirim</Button>
+      <Button onClick={closeCampaignModalFn} color="primary">Close</Button>
+    </DialogActions>
+  </Dialog>
   );
 };
 
