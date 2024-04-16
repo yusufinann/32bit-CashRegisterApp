cb743ffb88d378a5c8033430ddddeb7952333a98
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-16 20:23:34 +0300

    Update the Sales page design

--- src/HomePage/Sales.js
+++ src/HomePage/Sales.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import { Grid, Paper, Typography, useMediaQuery, useTheme, Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import { useNavigate } from 'react-router-dom';
 import TravelExploreTwoToneIcon from '@mui/icons-material/TravelExploreTwoTone';
 import GlobalNavi from "../GlobalComponents/GlobalNavi";
@@ -7,68 +8,59 @@ import ProductCatalog from "../SalesPage/ProductCatalog";
 import CartManagement from "../SalesPage/CartManagement";
 import TransactionButtons from "../SalesPage/TransactionButtons";
 
+const Item = styled(Paper)(({ theme }) => ({
+  textAlign: 'center',
+  overflow: "auto",
+  margin: theme.spacing(1),
+  border: "1px solid #2b2d42",
+  [theme.breakpoints.up('xs')]: {
+    height: '70vh',
+  },
+  [theme.breakpoints.up('sm')]: {
+    height: '85vh',
+  },
+  [theme.breakpoints.up('md')]: {
+    height: '85vh',
+  }
+}));
+
 const Sales = () => {
-    const navigate = useNavigate();
-    const somethingText = "See Price";
+  const navigate = useNavigate();
+  const theme = useTheme();
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up('md'));
+  
+  const handleIconClick = () => {
+    navigate('/price');
+  };
 
-    const handleIconClick = () => {
-        // İkon tıklandığında anasayfaya yönlendirme
-        navigate('/price');
-      };
   return (
-    <Grid container spacing={2}>
-       
+    <Grid container spacing={0.5}>
       <Grid item xs={12} style={{ border: "1px solid #2b2d42" }}>
-      <GlobalNavi  title="Sales Page" linkTo="/home" icon={<TravelExploreTwoToneIcon onClick={handleIconClick} style={{ fontSize: '3rem' }} />} something={somethingText} />
+        <GlobalNavi
+          title="Sales Page"
+          linkTo="/home"
+          icon={
+            <Button onClick={handleIconClick}>
+              <TravelExploreTwoToneIcon style={{ fontSize: '3rem' }} />
+            </Button>
+          }
+          something="See Price"
+        />
       </Grid>
-
       
-      <Grid
-        item
-        xs={12} // Full width on small screens
-        sm={4} // Five columns on medium screens and larger
-        style={{
-          margin: 10,
-          border: "1px solid #2b2d42",
-          borderRadius: 20,
-          height: "85vh", // Set a fixed height
-          overflow: "auto", // Enable scrolling
-        }}
-      >
-        <ProductCatalog/>
+      <Grid item xs={12} sm={4}>
+        <Item><ProductCatalog/></Item>
       </Grid>
-
-      {/* CartList */}
-      <Grid
-        item
-        xs={12} // Full width on small screens
-        sm={4} // Four columns on medium screens and larger
-        style={{
-          margin: 10,
-          border: "1px solid #2b2d42",
-          borderRadius: 20,
-          height: "85vh", // Set a fixed height
-          
-        }}
-      >
-        <CartManagement/>
-      </Grid>
-
      
-      <Grid
-        item
-        xs={12} // Full width on small screens
-        sm={3} // Two columns on medium screens and larger
-        style={{
-          margin: 10,
-          border: "1px solid #2b2d42",
-          borderRadius: 20,
-          height: "85vh", // Set a fixed height
-        }}
-      >
-        <div>
-        <TransactionButtons/>
-        </div>
+      <Grid item xs={12} sm={4}>
+        <Item><CartManagement/></Item>
+      </Grid>
+      
+      <Grid item xs={12} sm={4}>
+        <Item>
+          <Typography variant="h6">Transaction Panel</Typography>
+          <TransactionButtons/>
+        </Item>
       </Grid>
     </Grid>
   );
