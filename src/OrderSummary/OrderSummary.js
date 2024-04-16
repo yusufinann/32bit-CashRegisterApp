5350406baa7dbdce4ae0739b92590e7ebe8591b9
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-16 23:32:33 +0300

    update the OrderSummary page

--- src/OrderSummary/OrderSummary.js
+++ src/OrderSummary/OrderSummary.js
@@ -1,65 +1,51 @@
 import React from 'react'
-import { Grid } from "@mui/material";
-import GlobalNavi from '../GlobalComponents/GlobalNavi';
+import GlobalNavi from '../GlobalComponents/GlobalNavi'
+import { Grid,  Typography,Paper} from "@mui/material";
 import ReceiptArea from './ReceiptArea';
+import { styled } from "@mui/material/styles";
 import CartManagement from '../SalesPage/CartManagement';
 const OrderSummary = () => { //see price
-  return (
-    <Grid container spacing={2}>
-     {/*Navi */}
-    <Grid
-      item
-      xs={12}
-      style={{  border: "1px solid #2b2d42" }}
-    >
-      <GlobalNavi  title="See Price" linkTo="/sales" />
-    </Grid>
+
+  const Item = styled(Paper)(({ theme }) => ({
+    textAlign: 'center',
+    overflow: "auto",
+    margin: theme.spacing(1),
+    //borderRadius: 10,
+    border: "1px solid #2b2d42",
     
-    <Grid
-        item
-        xs={12} // Full width on small screens
-        sm={4} // Five columns on medium screens and larger
-        style={{
-          margin: 10,
-          border: "1px solid #2b2d42",
-          borderRadius: 20,
-          height: "85vh", // Set a fixed height
-          overflow: "auto", // Enable scrolling
-        }}
-      >
+    // Medya sorguları kullanarak responsive yükseklik ayarla
+    [theme.breakpoints.up('xs')]: {
+      height: '70vh', // Küçük cihazlarda
+    },
+    [theme.breakpoints.up('sm')]: {
+      height: '85vh', // Orta büyüklükteki cihazlarda
+    },
+    [theme.breakpoints.up('md')]: {
+      height: '85vh', // Büyük cihazlarda
+    }
+  }));
+  
+  return (
+<Grid container spacing={0.5}>
+      <Grid item xs={12} style={{border: "1px solid #2b2d42"}}>        
+       
+      <GlobalNavi  title="See Price" linkTo="/sales" />
+      </Grid>
+      <Grid item xs={12} sm={4} >
+        <Item>
         <ReceiptArea/>
+        </Item>
       </Grid>
-
-      {/* CartList */}
-      <Grid
-        item
-        xs={12} // Full width on small screens
-        sm={4} // Four columns on medium screens and larger
-        style={{
-          margin: 10,
-          border: "1px solid #2b2d42",
-          borderRadius: 20,
-          height: "85vh", // Set a fixed height
-          
-        }}
-      >
-        <CartManagement/>
+      <Grid item xs={12} sm={4}>
+        <Item>
+          <CartManagement />
+        </Item>
       </Grid>
-
-      <Grid
-        item
-        xs={12} // Full width on small screens
-        sm={3} // Two columns on medium screens and larger
-        style={{
-          margin: 10,
-          border: "1px solid #2b2d42",
-          borderRadius: 20,
-          height: "85vh", // Set a fixed height
-        }}
-      >
-        <div>
-          {/* component */}
-        </div>
+      <Grid item xs={12} sm={4}>
+        <Item>
+          <Typography variant="h6">İşlem Paneli</Typography>
+         {/* Right component */}
+        </Item>
       </Grid>
     </Grid>
   )
