f03582901eac12cbf3d174008f520d90497ce1d7
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-16 20:54:37 +0300

    Update the Cart Management component

--- src/SalesPage/CartManagement.js
+++ src/SalesPage/CartManagement.js
@@ -1,34 +1,32 @@
-import React from 'react';
+import React from "react";
 import { Grid } from "@mui/material";
-import CartList from './CartList';
-import TotalAmount from './TotalAmount';
-import { useGlobalContext } from '../contexts/GlobalContext';
+import CartList from "./CartList";
+import TotalAmount from "./TotalAmount";
+import { useGlobalContext } from "../contexts/GlobalContext";
 
 function CartManagement() {
-    const { totalAmount } = useGlobalContext();
-    return (
-        <div>
-            <Grid
-                item
-                xs={12} // Full width on small screens
-                sm={12} // Four columns on medium screens and larger
-                style={{
-                    
-                    height: "70vh", // Set to 80% of the viewport height
-                    overflow: "auto",
-                    backgroundColor: "yellow",
-                    borderTopLeftRadius: 20,
-                    borderTopRightRadius:20,
-                    width: "100%", // Full width
-                    marginTop:"auto"
-                }}
-            >
-                <CartList /> 
-            </Grid>
+  const { totalAmount } = useGlobalContext();
+  return (
+    <div style={{ margin: 20 }}>
+      <Grid
+        item
+        xs={12} // Full width on small screens
+        sm={12} // Four columns on medium screens and larger
+        style={{
+          height: "70vh", // Set to 80% of the viewport height
+          overflow: "auto",
+          backgroundColor: "white",
+          borderTopLeftRadius: 10,
+          borderTopRightRadius: 10,
+          width: "100%", // Full width
+        }}
+      >
+        <CartList />
+      </Grid>
 
-            <TotalAmount totalAmount={totalAmount} />      
-        </div>
-    );
+      <TotalAmount totalAmount={totalAmount} />
+    </div>
+  );
 }
 
 export default CartManagement;
