ff0f0b44fd11a17f76f636b4597a87dbc4025888
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-16 23:25:44 +0300

    update the filteredProductList

--- src/SalesPage/FilteredProductList.js
+++ src/SalesPage/FilteredProductList.js
@@ -1,61 +1,25 @@
 import React from 'react';
-    import PropTypes from 'prop-types';
-    import { Typography, Card, CardContent, CardMedia, Button } from '@mui/material'; // Import Material-UI components
-    import { useGlobalContext } from '../contexts/GlobalContext';
-    import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
-    import './styles.css';
-    
-    const FilteredProductList = () => {
-        const { state, addToCart } = useGlobalContext();
-      
-        const handleAddToCart = (product) => {
-          addToCart(product);
-        };
-      
-        const handleCardClick = (product) => {
-          addToCart(product);
-        };
-    
-      return (
+import PropTypes from 'prop-types';
+import { Typography } from '@mui/material'; // Import Material-UI components
+import { useGlobalContext } from '../contexts/GlobalContext';
+import './styles.css';
+import GlobalCardList from '../GlobalComponents/GlobalCardList';
+
+const FilteredProductList = () => {
+    const { state,handleAddToCart } = useGlobalContext();
+
+  
+    return (
         <div>
-          <div className="card-container">
-            {state.filteredProducts.map((product, index) => (
-              <Card key={product.product_id} sx={{ width: 200 }} className="custom-card" onClick={() => handleCardClick(product)}>
-                <CardMedia
-                  component="img"
-                  alt={product.product_name}
-                  height="140"
-                  image={product.image_url}
-                />
-                <CardContent>
-                  <Typography variant="subtitle1" color="text.primary" fontWeight="bold">
-                    {product.name}
-                  </Typography>
-                  <Typography variant="body1">
-                    Price: {product.price} TL
-                  </Typography>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    startIcon={<AddShoppingCartIcon />}
-                    onClick={(e) => {
-                      e.stopPropagation(); // Stop the card's onClick event
-                      handleAddToCart(product);
-                    }}
-                  >
-                    Sepete Ekle
-                  </Button>
-                </CardContent>
-              </Card>
-            ))}
-          </div>
+            <Typography variant="h5" gutterBottom>
+                Filtered Products
+            </Typography>
+            <GlobalCardList array={state.filteredProducts} AddToCartFunction={handleAddToCart} />          
         </div>
-      );
-    };
-    
-    FilteredProductList.propTypes = {
-      filteredProducts: PropTypes.array.isRequired,
-    };
-    
-    export default FilteredProductList;
-    
+    );
+   
+};
+FilteredProductList.propTypes = {
+    filteredProducts: PropTypes.array.isRequired,
+  };
+export default FilteredProductList;
