70ee93da0f9dbdc5dd1312dfb01f48d40bc95feb
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-20 12:17:07 +0300

    Update Filtered Product List to get rid of console error

--- src/SalesPage/FilteredProductList.js
+++ src/SalesPage/FilteredProductList.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Typography } from '@mui/material'; // Import Material-UI components
 import { useGlobalContext } from '../contexts/GlobalContext';
 import './styles.css';
 import GlobalCardList from '../GlobalComponents/GlobalCardList';
 
 const FilteredProductList = () => {
-    const { state,handleAddToCart } = useGlobalContext();
+    const { state, handleAddToCart } = useGlobalContext();
 
-  
     return (
         <div>
             <Typography variant="h5" gutterBottom>
                 Filtered Products
             </Typography>
-            <GlobalCardList array={state.filteredProducts} AddToCartFunction={handleAddToCart} />          
+            <GlobalCardList array={state.filteredProducts} AddToCartFunction={handleAddToCart} />
         </div>
     );
-   
 };
-FilteredProductList.propTypes = {
-    filteredProducts: PropTypes.array.isRequired,
-  };
+
 export default FilteredProductList;
