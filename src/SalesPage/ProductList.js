fecd2da66841d74858e30d642bc8fee0412bcd40
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-28 16:05:53 +0300

    Update the ProductList and use GlobalContext in here

--- src/SalesPage/ProductList.js
+++ src/SalesPage/ProductList.js
@@ -1,24 +1,23 @@
 
 import React from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import './styles.css';
 import { useGlobalContext } from '../contexts/GlobalContext';
 import GlobalCardList from '../GlobalComponents/GlobalCardList';
 
 
-
-const ProductList = ({ products }) => {
-  const {handleAddToCart } = useGlobalContext();
+const ProductList = () => {
+  const {handleAddToCart,state } = useGlobalContext();
 
   return (
     <div>
-        <GlobalCardList array={products} AddToCartFunction={handleAddToCart} />
+        <GlobalCardList array={state.products} AddToCartFunction={handleAddToCart} />
     </div>
   );
 };
 
-ProductList.propTypes = {
-  products: PropTypes.array.isRequired,
-};
+// ProductList.propTypes = {
+//   products: PropTypes.array.isRequired,
+// };
 
 export default ProductList;
