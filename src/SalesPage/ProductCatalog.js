f3073c7c49e41e8e5252d454756edbb39e7cab5c
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-28 16:01:16 +0300

    Update the ProductCatalog component

--- src/SalesPage/ProductCatalog.js
+++ src/SalesPage/ProductCatalog.js
@@ -1,70 +1,83 @@
-import React, { useEffect } from 'react';
-import { TextField, Button} from '@mui/material';
+import React, { useCallback, useState } from 'react';
+import { Button, TextField, IconButton } from '@mui/material';
+import ClearIcon from '@mui/icons-material/Clear';
 import './styles.css'
 import { useGlobalContext } from '../contexts/GlobalContext';
 import CategoryList from './CategoryList';
 import ProductList from './ProductList';
 import FilteredProductList from './FilteredProductList';
+import VirtualKeyboard from '../GlobalComponents/VirtualKeyboard';
 
 const ProductCatalog = () => {
 
-    const {state, setState, handleShowCategories, handleShowProducts, handleBarcodeChange,  handleShowProductsByCategoryId } = useGlobalContext();
-    useEffect(() => {
-      handleShowCategories();
-    }, [handleShowCategories]);
-    useEffect(() => {
-      handleShowProducts();
-    }, [handleShowProducts]);
-  return (
-    <div>
-      <form>
-        <TextField
-          id="barcodeInput"
-          label="Barkod giriniz"
-          variant="outlined"
-          value={state.barcode}
-          onChange={handleBarcodeChange}
-          fullWidth
-          style={{ marginTop: 10 }}
-        />
-        <div className="button-container">
-          <Button
-            variant="contained"
-            color="primary"            
-            onClick={handleShowCategories}
-          >
-            Kategoriler
-          </Button>
-          <Button variant="contained" color="success"  onClick={() => {}}>
-            Alt Kategoriler
-          </Button>
-          <Button
-            variant="contained"
-            color="error"
-            onClick={handleShowProducts}
-          >
-            Ürünler
-          </Button>
-        </div>
-      </form>
-      {(state.showFilteredProducts && state.barcode) && (
-        <FilteredProductList />
-      )}
+  const { state, setState, handleShowCategories, handleShowProducts, handleBarcodeChange,setIsKeyboardOpen ,setKeyboardPosition} = useGlobalContext();
+  const [inputValue, setInputValue] = useState("");
+   
+  const handleInputClick = (event) => {
+    const rect = event.target.getBoundingClientRect();
+    setIsKeyboardOpen(true);
+    setKeyboardPosition({ x: rect.left, y: rect.bottom });
+  };
 
-      {state.showCategories && (
-        <CategoryList  //CategoryList
-          categories={state.categories}
-          handleShowProducts={handleShowProductsByCategoryId}
-        />
-      )}
-        {state.showProducts && (
-        <ProductList
-          products={state.products}
-          isOpen={state.showProducts}
-          toggle={() => setState({ ...state, showProducts: !state.showProducts })}
-        />
-      )}
+  const toggleProducts = useCallback(() => {
+    setState(prevState => ({ ...prevState, showProducts: !prevState.showProducts }));
+  }, [setState]);
+ 
+  const toggleCategories = useCallback(() => {
+    setState(prevState => ({ ...prevState, showCategories: !prevState.showCategories }));
+  }, [setState]);
+
+  const handleVirtualKeyboardPress = useCallback((value) => {
+    handleBarcodeChange({ target: { value } });
+    setInputValue(value);
+  }, [handleBarcodeChange]);
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+    handleBarcodeChange({ target: { value } });
+  };
+  return (
+    <> 
+    <TextField
+      value={inputValue}
+      placeholder={"Giriş için tıklayın..."}
+      onClick={handleInputClick}
+      onChange={handleInputChange}
+      fullWidth
+      variant="outlined"
+      InputProps={{
+        style: { cursor: "pointer", color: "#dc143c" }, // kırmızı metin rengi
+        endAdornment: (
+          <IconButton onClick={() => setInputValue("")} style={{ color: "#dc143c" }}>
+            <ClearIcon /> {/* Temizleme simgesi */}
+          </IconButton>
+        ),
+      }}
+      InputLabelProps={{
+        style: { color: "#dc143c" }, // kırmızı etiket rengi
+      }}
+    />
+    <VirtualKeyboard
+      onInputChange={setInputValue}
+      KeyboardPress={handleVirtualKeyboardPress}
+    />
+    <div className="button-container">
+      <Button variant="contained" color="primary" onClick={handleShowCategories}>
+        Kategoriler
+      </Button>
+      <Button variant="contained" color="success">
+        Alt Kategoriler
+      </Button>
+      <Button variant="contained" color="error" onClick={handleShowProducts}>
+        Ürünler
+      </Button>
     </div>
+    {state.showFilteredProducts && state.barcode && <FilteredProductList />}
+    {state.showCategories && <CategoryList isOpen={state.showCategories} toggle={toggleCategories} />}
+    {state.showProducts && <ProductList isOpen={state.showProducts} toggle={toggleProducts} />}
+   
+  </>
   );
 };
 
