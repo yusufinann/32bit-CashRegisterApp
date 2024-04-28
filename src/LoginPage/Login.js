ce3800022f191b9253fda064e2be4c49d554db0e
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-28 16:22:10 +0300

    Update the Login component and implement the VirtualKeyboard

--- src/LoginPage/Login.js
+++ src/LoginPage/Login.js
@@ -10,14 +10,14 @@ import {
   Box,
   IconButton,
   InputAdornment,
-  Modal,
-  Paper,
 } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { Visibility, VisibilityOff, Keyboard } from '@mui/icons-material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useLogin } from '../contexts/LoginContext';
 import { getVersion } from '../services/versionService'; // Import the getVersion function
+import { useGlobalContext } from '../contexts/GlobalContext';
+import VirtualKeyboard from '../GlobalComponents/VirtualKeyboard';
 
 const theme = createTheme();
 
@@ -26,33 +26,52 @@ const Login = () => {
   const usernameInputRef = useRef(null);
   const passwordInputRef = useRef(null);
   const [showPassword, setShowPassword] = useState(false);
-  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
+  const { setIsKeyboardOpen } = useGlobalContext();
+  const [inputTarget, setInputTarget] = useState(null);
+
   const version = getVersion();
 
-  const handleLogin = () => {
+  const [inputUser, setInputUser] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
+  const [keyboardPosition, setKeyboardPosition] = useState({ x: 0, y: 0 });
+
+  const handleLogin = (event) => {
+    event.preventDefault();
     login(usernameInputRef.current.value, passwordInputRef.current.value);
   };
 
-  const togglePasswordVisibility = () => {
-    setShowPassword((prevShowPassword) => !prevShowPassword);
+  const handleInputClick = (event) => {
+    const inputName = event.target.name;
+    setInputTarget(inputName);
+    if (inputName === 'username' || inputName === 'password') {
+      setIsKeyboardOpen(true);
+      const screenWidth = window.innerWidth;
+      const screenHeight = window.innerHeight;
+      setKeyboardPosition({ x: (screenWidth - 800) / 2, y: (screenHeight - 800) / 2 });
+    }
   };
 
-  const openKeyboard = () => {
-    setIsKeyboardOpen(true);
-  };
-
-  const closeKeyboard = () => {
-    setIsKeyboardOpen(false);
+const togglePasswordVisibility = () => {
+    setShowPassword(prev => !prev);
   };
 
   if (isLoggedIn) {
     return <Navigate to="/home" />;
   }
 
+  const handleVirtualKeyboardPressUser = (value) => {
+    setInputUser(value);
+  };
+
+  const handleVirtualKeyboardPressPassword = (value) => {
+    setInputPassword(value);
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
+        <form onSubmit={handleLogin}>
         <Box
           sx={{
             display: 'flex',
@@ -74,6 +93,9 @@ const Login = () => {
             Login
           </Typography>
           <TextField
+            value={inputUser}
+            onClick={handleInputClick}
+            onChange={(e) => setInputUser(e.target.value)}
             variant="outlined"
             margin="normal"
             required
@@ -83,17 +105,29 @@ const Login = () => {
             name="username"
             autoFocus
             inputRef={usernameInputRef}
+            autoComplete="username"
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
-                  <IconButton onClick={openKeyboard}>
+                  <IconButton onClick={handleInputClick}  name="username">
                     <Keyboard />
                   </IconButton>
                 </InputAdornment>
               ),
             }}
           />
+          {inputTarget === 'username' && (
+              <VirtualKeyboard
+                inputValue={inputUser}
+                onInputChange={setInputUser}
+                KeyboardPress={handleVirtualKeyboardPressUser}
+                position={keyboardPosition}
+              />
+            )}
           <TextField
+          value={inputPassword}
+          onClick={handleInputClick}
+          onChange={(e) => setInputPassword(e.target.value)}
             variant="outlined"
             margin="normal"
             required
@@ -108,13 +142,22 @@ const Login = () => {
                   <IconButton onClick={togglePasswordVisibility}>
                     {showPassword ? <VisibilityOff /> : <Visibility />}
                   </IconButton>
-                  <IconButton onClick={openKeyboard}>
+                  <IconButton onClick={handleInputClick} name="password">
                     <Keyboard />
                   </IconButton>
                 </InputAdornment>
               ),
             }}
           />
+          {inputTarget === 'password' && (
+              <VirtualKeyboard
+                inputValue={inputPassword}
+                onInputChange={setInputPassword}
+                KeyboardPress={handleVirtualKeyboardPressPassword}
+                position={keyboardPosition}
+              />
+            )}
+
           {showError && (
             <Typography variant="body2" color="error">
               Invalid username or password
@@ -135,30 +178,8 @@ const Login = () => {
             </Typography>
           )}
         </Box>
+        </form>
       </Container>
-      <Modal
-        open={isKeyboardOpen}
-        onClose={closeKeyboard}
-        aria-labelledby="keyboard-modal-title"
-        aria-describedby="keyboard-modal-description"
-      >
-        <Paper
-          style={{
-            position: 'absolute',
-            width: 400,
-            backgroundColor: '#fff',
-            padding: 20,
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            borderRadius: '10px',
-          }}
-        >
-          <Typography id="keyboard-modal-title" variant="h6" component="div">
-            Virtual Keyboard
-          </Typography>
-        </Paper>
-      </Modal>
     </ThemeProvider>
   );
 };
