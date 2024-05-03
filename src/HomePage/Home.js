11c9fb90a12741a5968cb68cd286bfb1fee511e0
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-05-03 20:56:30 +0300

    Update the Home.js component

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Container, Typography, Grid, styled} from '@mui/material';
+import { Container, Typography, Grid, styled, Paper, Box, Avatar, Button} from '@mui/material';
 import { LocalGroceryStore,Assignment,MonetizationOn, CollectionsBookmark,AlarmOn, Settings   } from '@mui/icons-material';
 import MenuButtons from '../GlobalComponents/MenuButtons';
+import { useLogin } from '../contexts/LoginContext';
 
 
 
@@ -27,10 +28,53 @@ const TitleTypography = styled(Typography)({
     color: 'black',
     textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
 });
-
+const AvatarContainer = styled(Box)({
+    position: 'relative',
+    marginBottom: '20px',
+});
 
 const Home = () => {
+    const { isLoggedIn, user } = useLogin();
+
     return (
+        <div>
+        <Paper
+            style={{
+                position: 'absolute',
+                top: '10px',
+                left: '10px',
+                padding: '20px',
+                backgroundColor: '#ffffff',
+                boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+            }}
+        >
+            {isLoggedIn ? (
+                <Box textAlign="center">
+                    <AvatarContainer>
+                        <Avatar alt={user.personelInfo.name} src="/static/images/avatar/1.jpg" sx={{ width: 100, height: 100, margin: '0 auto' }} />
+                     {/* <LogoutButton/> */}
+                    </AvatarContainer>
+                    <Typography variant="h6" gutterBottom mt={2}>
+                        Hoş geldiniz, {user.username}!
+                    </Typography>
+                    <Typography variant="subtitle1" gutterBottom>
+                        Personel: {user.personelInfo.name}
+                    </Typography>
+                    <Typography variant="subtitle1" gutterBottom>
+                        Kasa Konumu: {user.kasaInfo.location}
+                    </Typography>
+                    <Button variant="contained" color="primary" size="large" style={{ marginTop: '20px' }}>
+                        Profili Düzenle
+                    </Button>
+                </Box>
+            ) : (
+                <Typography variant="body1" gutterBottom>
+                    Lütfen giriş yapın
+                </Typography>
+            )}
+        </Paper>
+
+
         <MainContainer component="main" maxWidth="md">
              <TitleTypography gutterBottom>
                 Welcome to Home Page
@@ -79,6 +123,7 @@ const Home = () => {
               {/* Menu Buttons */}
                 </Grid>
                 </MainContainer>
+                </div>
     );
 }
 
