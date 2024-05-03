f301495a9d05ad8f6e6e7cb406521060c5fbdc64
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-05-03 21:10:51 +0300

    Updated LogoutButton.js

--- src/LoginPage/LogoutButton.js
+++ src/LoginPage/LogoutButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IconButton ,styled,Box,Tooltip} from '@mui/material';
-import { useLogin } from './contexts/LoginContext';
+import { useLogin } from '../contexts/LoginContext';
 import { useNavigate } from 'react-router-dom';
 import {ExitToApp } from '@mui/icons-material';
 
