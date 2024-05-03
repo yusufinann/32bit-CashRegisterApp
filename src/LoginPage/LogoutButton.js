1e37ec77913acad8a9014117245281c828937ca2
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-05-03 20:58:07 +0300

    Create and update LogoutButton component

---
+++ src/LoginPage/LogoutButton.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { IconButton ,styled,Box,Tooltip} from '@mui/material';
+import { useLogin } from './contexts/LoginContext';
+import { useNavigate } from 'react-router-dom';
+import {ExitToApp } from '@mui/icons-material';
+
+
+const LogoutButton = () => {
+  const {logout } = useLogin();
+
+  const LogoutButtonContainer = styled(Box)({
+    position: 'absolute',
+    top: '5px',
+    right: '5px',
+});
+
+
+  const navigate = useNavigate(); // useNavigate hook'unu kullanarak navigate fonksiyonunu alın
+
+  const handleLogout = () => {
+    logout();
+    navigate('/');
+  };
+
+  return (
+    <LogoutButtonContainer>
+        <Tooltip title="Exit">
+    <IconButton color="primary" onClick={handleLogout}>
+        <ExitToApp sx={{ fontSize: '32px' }} />
+    </IconButton>
+    </Tooltip>
+</LogoutButtonContainer>
+  );
+};
+
+export default LogoutButton;
